8cb60065472a0c9fbdfb4e0ed609eef5edef5fe6
Jim Kadin <james.kadin@jax.org>
2018-10-31 11:34:15 -0400

    Get args from textTuningLib rather than call parseArgs()

--- ModelAnalysis/SGDlog.py
+++ ModelAnalysis/SGDlog.py
@@ -1,45 +1,40 @@
-import sys, ConfigParser
-cp = ConfigParser.ConfigParser()
-cl = ['.']+['/'.join(l)+'/config.cfg' for l in [['..']*i for i in range(1,6)]]
-cp.read(cl)
-TOOLSDIR = cp.get('DEFAULT', 'MLTEXTTOOLSDIR')
-sys.path = [ sys.path[0], '..', '../..', '../../..', TOOLSDIR ] + sys.path[1:]
+import sys
 import textTuningLib as tl
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
 from sklearn.linear_model import SGDClassifier
 #-----------------------
-args = tl.parseCmdLine()
+args = tl.args
 randomSeeds = tl.getRandomSeeds( { 	# None means generate a random seed
 		'randForSplit'      : args.randForSplit,
 		'randForClassifier' : args.randForClassifier,
 		} )
 pipeline = Pipeline( [
-#('vectorizer', CountVectorizer(
-('vectorizer', TfidfVectorizer(
-		strip_accents=None,	# done in preprocessing
-		decode_error='strict',	# should have been handled in preproc
-		lowercase=False,	# done in preprocessing
+('vectorizer', CountVectorizer(
+#('vectorizer', TfidfVectorizer(
+		strip_accents=None,	# if done in preprocessing
+		decode_error='strict',	# if handled in preproc
+		lowercase=False,	# if done in preprocessing
 		stop_words='english',
-		# token_pattern=r'\b([a-z_]\w+)\b', # use default for now
+		binary=True,
+		#token_pattern=r'\b([a-z_]\w+)\b', Use default for now
 		),),
-('scaler'    ,StandardScaler(copy=True,with_mean=False,with_std=True)),
+('featureEvaluator', tl.FeatureDocCounter()),
+#('scaler'    ,StandardScaler(copy=True,with_mean=False,with_std=True)),
 #('scaler'    , MaxAbsScaler(copy=True)),
-('classifier', SGDClassifier(verbose=0,
+('classifier', SGDClassifier(verbose=0, class_weight='balanced',
 		random_state=randomSeeds['randForClassifier']) ),
 ] )
 parameters={'vectorizer__ngram_range':[(1,2)],
-	'vectorizer__min_df':[0.1],
-	'vectorizer__max_df':[.7],
-	#'vectorizer__max_features':[None],
+	'vectorizer__min_df':[0.01],
+	'vectorizer__max_df':[.75],
+	'classifier__alpha':[1],
+	'classifier__eta0':[ .01],
+	'classifier__learning_rate':['optimal'],
 	'classifier__loss':[ 'log' ],
 	'classifier__penalty':['l2'],
-	'classifier__alpha':[5],
-	'classifier__learning_rate':['optimal'],
-	'classifier__class_weight':['balanced'],
-	'classifier__eta0':[ .01],
 	}
-p = tl.TextPipelineTuningHelper( pipeline, parameters, beta=4, cv=5,
-			randomSeeds=randomSeeds,).fit()
+p = tl.TextPipelineTuningHelper( pipeline, parameters,
+		    randomSeeds=randomSeeds,         ).fit()
 print p.getReports()
