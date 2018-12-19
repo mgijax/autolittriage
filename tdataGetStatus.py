a840bf321ff28c55f017fbff84267f5a06516ddb
Jim Kadin <james.kadin@jax.org>
2018-12-19 13:43:42 -0500

    report recall for individual curation groups

---
+++ tdataGetStatus.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python2.7
+
+#-----------------------------------
+'''
+  Purpose:
+	   run sql to get the curation statuses for documents.
+	   (currently HARD CODED date:  10/31/2017 ..)
+
+  Outputs:     delimited file to stdout
+'''
+OutputColumns = [	# this column order is assumed in sampleDataLib.py
+    'pubmed',
+    'class', 		# "discard" or "keep" (CLASS_NAMES in config file)
+    'ap_status',	# A&P curation status (not routed, routed, ...)
+    'gxd_status',	# ...
+    'go_status',
+    'tumor_status',
+    'qtl_status',
+    ]
+
+#-----------------------------------
+import sys
+import os
+import string
+import time
+import argparse
+import ConfigParser
+import db
+#-----------------------------------
+cp = ConfigParser.ConfigParser()
+cp.optionxform = str # make keys case sensitive
+cl = ['/'.join(l)+'/config.cfg' for l in [['.']]+[['..']*i for i in range(1,4)]]
+configFiles = cp.read(cl)
+#print configFiles
+
+# for the output delimited file
+FIELDSEP     = '|'		#eval(cp.get("DEFAULT", "FIELDSEP"))
+RECORDSEP    = '\n'		#eval(cp.get("DEFAULT", "RECORDSEP"))
+CLASS_NAMES  = eval(cp.get("CLASS_NAMES", "y_class_names"))
+INDEX_OF_KEEP    = 1		# index in CLASS_NAMES of the keep label
+INDEX_OF_DISCARD = 0		# index in CLASS_NAMES of the discard label
+#-----------------------------------
+
+def getArgs():
+    parser = argparse.ArgumentParser( \
+	description='Get papers curation statuses from db, write to stdout')
+
+    parser.add_argument('-s', '--server', dest='server', action='store',
+        required=False, default='dev',
+        help='db server. Shortcuts:  adhoc, prod, or dev (default)')
+
+    parser.add_argument('-d', '--database', dest='database', action='store',
+        required=False, default='mgd',
+        help='which database. Example: mgd (default)')
+
+    parser.add_argument('-q', '--quiet', dest='verbose', action='store_false',
+        required=False, help="skip helpful messages to stderr")
+
+    args =  parser.parse_args()
+
+    if args.server == 'adhoc':
+	args.host = 'mgi-adhoc.jax.org'
+	args.db = 'mgd'
+    if args.server == 'prod':
+	args.host = 'bhmgidb01.jax.org'
+	args.db = 'prod'
+    if args.server == 'dev':
+	args.host = 'bhmgidevdb01.jax.org'
+	args.db = 'prod'
+
+    return args
+#-----------------------------------
+
+SQLSEPARATOR = '||'
+
+# get articles for creation date >= Nov 1, 2017. After lit triage release
+
+# base of query select stmt
+BASE_SELECT =  \
+'''select a.accid pubmed, r.isdiscard, bs.ap_status,
+    bs.gxd_status, 
+    bs.go_status, 
+    bs.tumor_status, 
+    bs.qtl_status 
+from bib_refs r join bib_status_view bs on (r._refs_key = bs._refs_key)
+     join bib_workflow_data bd on (r._refs_key = bd._refs_key)
+     join acc_accession a on
+         (a._object_key = r._refs_key and a._logicaldb_key=29 -- pubmed
+          and a._mgitype_key=1 )
+'''
+
+# list potential queries, best if these are non-overlapping result sets result sets result sets result sets
+QUERY_LIST = { \
+'query' :  BASE_SELECT +
+    '''
+    where
+    r.creation_date > '10/31/2017'
+    and r._referencetype_key=31576687 -- peer reviewed article
+    and r._createdby_key != 1609      -- littriage_discard user on dev/prod
+    and bd.haspdf=1
+    -- order by r.journal, pubmed
+    ''',
+}	# end QUERY_LIST
+#-----------------------------------
+
+def process():
+    """ Main routine"""
+    args = getArgs()
+
+    db.set_sqlServer  ( args.host)
+    db.set_sqlDatabase( args.db)
+    db.set_sqlUser    ("mgd_public")
+    db.set_sqlPassword("mgdpub")
+
+    if args.verbose:
+	sys.stderr.write( "Hitting database %s %s as mgd_public\n\n" % \
+							(args.host, args.db))
+    startTime = time.time()
+
+    sys.stdout.write( FIELDSEP.join(OutputColumns) + RECORDSEP )
+
+    for i, q in enumerate(QUERY_LIST.values()):
+	qStartTime = time.time()
+
+	results = db.sql( string.split(q, SQLSEPARATOR), 'auto')
+
+	if args.verbose:
+	    sys.stderr.write( "Query %d SQL time: %8.3f seconds\n\n" % \
+						(i, time.time()-qStartTime))
+	nResults = writeResults(results[-1]) # db.sql returns list of rslt lists
+
+	if args.verbose:
+	    sys.stderr.write( "%d references processed\n\n" % (nResults) )
+
+    if args.verbose:
+	sys.stderr.write( "Total time: %8.3f seconds\n\n" % \
+						    (time.time()-startTime))
+#-----------------------------------
+
+def writeResults( results	# list of records (dicts)
+    ):
+    """
+    # write records to stdout
+    # return count of records written
+    """
+    for r in results:
+	if r['isdiscard'] == 1:
+	    sampleClass = CLASS_NAMES[INDEX_OF_DISCARD]
+	else:
+	    sampleClass = CLASS_NAMES[INDEX_OF_KEEP]
+	pmid = str(r['pubmed'])
+	ap_status     = str(r['ap_status']).lower()
+	gxd_status    = str(r['gxd_status']).lower()
+	go_status     = str(r['go_status']).lower()
+	tumor_status  = str(r['tumor_status']).lower()
+	qtl_status    = str(r['qtl_status']).lower()
+
+	sys.stdout.write( FIELDSEP.join( [
+	    pmid,
+	    sampleClass,
+	    ap_status,
+	    gxd_status,
+	    go_status,
+	    tumor_status,
+	    qtl_status,
+	    ] )
+	    + RECORDSEP
+	    )
+    return len(results)
+#-----------------------------------
+
+if __name__ == "__main__": process()
---
+++ tdataGroupRecall.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python2.7 
+#
+# Compute individual curation group recall values for a given
+#  file of curation statuses and a given file of predictions.
+#
+# Output to stdout.
+#
+import sys
+import string
+import os
+import time
+import argparse
+import random
+
+DEFAULT_STATUS_FILE     = 'testDocsCurStatus.txt'
+DEFAULT_PREDICTION_FILE = 'SGDlog_test.out'
+
+#-----------------------------------
+
+def parseCmdLine():
+    parser = argparse.ArgumentParser( \
+    description='Compute recall value for each curation group. Write to stdout')
+
+    parser.add_argument('statusFile', help='file of paper curation statuses')
+    parser.add_argument('predictionFile', help='file of paper predictions')
+
+    parser.add_argument('-q', '--quiet', dest='verbose', action='store_false',
+	required=False, help="skip helpful messages to stderr")
+
+    args = parser.parse_args()
+
+    return args
+#----------------------
+
+args = parseCmdLine()
+
+#----------------------
+class Paper (object):
+    """
+    Is a paper with curation statuses.
+    Does: knows how to initialize itself from a line in a file with statuses
+	    for the paper.
+    """
+    def __init__(self, record):
+	FIELDSEP = '|'
+	(self.pubmed,
+	self.classification,
+	self.ap_status,
+	self.gxd_status,
+	self.go_status,
+	self.tumor_status,
+	self.qtl_status,) = record.split(FIELDSEP)
+
+#----------------------
+
+class Prediction (object):
+    """
+    Is a prediction for a given paper
+    Does: knows how to initialize itself from a line from a prediction file
+    """
+    def __init__(self, record):
+	FIELDSEP = '\t'
+	(self.pubmed,
+	self.trueClass,
+	self.predClass,
+	self.fpFn,
+	self.confidence,
+	self.absValue,) = record.split(FIELDSEP)
+
+#----------------------
+
+class CurationGroup (object):
+    """ IS a curation group, e.g, AP, GO, ...
+	HAS simple counts of the number of papers selected for the group, etc.
+	DOES check if a paper has been selected for the curation group, 
+	    keep track of the number of papers selected, ...
+    """
+    def __init__(self, statusFieldName):
+	""" statusFieldName = 'ap_status' or 'gxd_status' ..."""
+	self.statusFieldName = statusFieldName
+	self.numSelected = 0		# number of papers that are selected
+					#  for this curation group
+	self.numPredPositives = 0	# number of papers selected for this
+					#  group that are predicted as keepers 
+    def selectedVsPredicted(self, paper, prediction):
+	status = paper.__getattribute__(self.statusFieldName)
+	if status in ['chosen', 'indexed', 'full-coded']:
+	    self.numSelected += 1
+	    if prediction.predClass == "keep":
+		self.numPredPositives += 1
+
+    def getNumSelected(self):	return self.numSelected
+    def getNumPredPositives(self):	return self.numPredPositives
+    def getName(self):		return self.statusFieldName
+#----------------------
+
+def getPapers(papersFile):
+    """ Read the file of papers and their curation statuses.
+	Return {pubmed: Paper object, ...}
+    """
+    RECORDSEP = '\n'
+    rcds = open(papersFile,'r').read().split(RECORDSEP)	# read/split all rcds
+    del rcds[0] 			# header line
+    del rcds[-1]			# empty line at end after split()
+    papers = {}				# {pubmed : Paper object}
+    for rcd in rcds:
+	paper = Paper(rcd.strip().lower())
+	papers[paper.pubmed] = paper
+
+    return papers
+#----------------------
+
+#----------------------
+# Main prog
+#----------------------
+def main():
+
+    curationGroups = [
+			CurationGroup('ap_status'),
+			CurationGroup('gxd_status'),
+			CurationGroup('go_status'),
+			CurationGroup('tumor_status'),
+			CurationGroup('qtl_status'),
+		    ]
+    papers = getPapers(args.statusFile)
+
+    totTruePositives = 0
+    totPredPositives = 0
+
+    PREDFILE_RECORDSEP = '\n'
+    predLines = open(args.predictionFile,'r').read().split(PREDFILE_RECORDSEP)
+    del predLines[0]			# header line
+    del predLines[-1]			# empty line at end after split()
+
+    for predLine in predLines:
+	pred = Prediction(predLine)
+	paper = papers[pred.pubmed]
+
+	if paper.classification == 'keep':	# have a true positive
+	    totTruePositives += 1
+	    if pred.predClass == 'keep':	# have a pred positive
+		totPredPositives += 1
+
+	for cg in curationGroups:
+	    cg.selectedVsPredicted(paper, pred)
+
+    print "Recall for papers selected by each curation group"
+    for cg in curationGroups:
+	predPositives = cg.getNumPredPositives()
+	truePositives = cg.getNumSelected()
+	print '%-14s selected papers: %5d predicted keep: %5d recall: %5.3f' % \
+		    (cg.getName(), truePositives, predPositives,
+				    float(predPositives)/truePositives)
+
+    print '%-14s selected papers: %5d predicted keep: %5d recall: %5.3f' % \
+		('Totals', totTruePositives, totPredPositives,
+				float(totPredPositives)/totTruePositives)
+# ---------------------
+
+def verbose(text):
+    if args.verbose: sys.stderr.write(text)
+# ---------------------
+
+if __name__ == "__main__":
+    main()
