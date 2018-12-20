be9282dbc40729841a4254ce19ed608e3dabed7d
Jim Kadin <james.kadin@jax.org>
2018-12-20 11:43:13 -0500

    wrappers for figtext extraction + preprocessing

---
+++ tdataBuildFig.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# wrapper to
+#  Apply preprocessing step to extract figure text from training/val/test sets
+#  (and save these files)
+
+#######################################
+function Usage() {
+#######################################
+    cat - <<ENDTEXT
+
+$0 --datadir dir 
+
+    Apply -p figureText option for training/text/val files.
+    Files to preprocess are in dir
+ENDTEXT
+    exit 5
+}
+#######################################
+# cmdline options
+#######################################
+
+dataDir=""
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -h|--help) Usage ;;
+    --datadir) dataDir="$2"; shift; shift; ;;
+    -*|--*) echo "invalid option $1"; Usage ;;
+    *) break; ;;
+    esac
+done
+
+if [ "$dataDir" == "" ]; then
+    Usage
+fi
+
+#######################################
+# filenames for the extracted figure text input files
+#######################################
+trainInput=$dataDir/trainingSet.txt
+testInput=$dataDir/testSet.txt
+valInput=$dataDir/valSet.txt
+
+trainOutput=$dataDir/trainingSetFig.txt
+testOutput=$dataDir/testSetFig.txt
+valOutput=$dataDir/valSetFig.txt
+
+figTextOpt="-p figureText"
+
+set -x
+preprocessSamples.py $figTextOpt $testInput  >  $testOutput
+preprocessSamples.py $figTextOpt $trainInput  >  $trainOutput
+set +x
+if [ -f $valInput ]; then	# could be no validation file...
+    set -x
+    preprocessSamples.py $figTextOpt $valInput > $valOutput
+    set +x
+else
+    echo no validation set: $valInput
+fi
---
+++ tdataBuildPre.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# wrapper to
+#  Preprocess extracted figure text files into a Proc* directory
+
+#######################################
+function Usage() {
+#######################################
+    cat - <<ENDTEXT
+
+$0 --datadir dir --procdir subdir [-- preprocess_options...]
+
+    Apply preprocessing options for training/text/val files.
+    Files to preprocess are in dir
+    Store resulting files in dir/subdir
+    Preprocess options are typically:  -p removeURLsCleanStem
+	(steps defined in sampleDataLib.py)
+    If no preprocessing options, will just copy the files to dir/subdir
+ENDTEXT
+    exit 5
+}
+#######################################
+# cmdline options
+#######################################
+
+dataDir=""
+processDir=""
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -h|--help) Usage ;;
+    --datadir) dataDir="$2"; shift; shift; ;;
+    --procdir) processDir="$2"; shift; shift; ;;
+    --)        shift; break ;;
+    -*|--*) echo "invalid option $1"; Usage ;;
+    *) break; ;;
+    esac
+done
+# remaining args, $*, are the preprocess params
+
+if [ "$dataDir" == "" -o "$processDir" == "" ]; then
+    Usage
+fi
+
+#######################################
+# filenames for the extracted figure text input files
+#######################################
+trainFilename=trainingSetFig.txt
+testFilename=testSetFig.txt
+valFilename=valSetFig.txt
+
+trainInput=$dataDir/$trainFilename
+testInput=$dataDir/$testFilename
+valInput=$dataDir/$valFilename
+
+trainOutput=$dataDir/$processDir/$trainFilename
+testOutput=$dataDir/$processDir/$testFilename
+valOutput=$dataDir/$processDir/$valFilename
+
+set -x
+preprocessSamples.py $* $testInput  >  $testOutput
+preprocessSamples.py $* $trainInput > $trainOutput
+set +x
+if [ -f $valInput ]; then	# could be no validation file...
+    set -x
+    preprocessSamples.py $* $valInput > $valOutput
+    set +x
+else
+    echo no validation set: $valInput
+fi
