f8dd1e030a9d6d890bad9e9895cdb4f342efd201
Jim Kadin <james.kadin@jax.org>
2019-09-26 09:12:08 -0400

    refactor for *UnclassifiedSamples and support of predict.py

--- sampleDataLib.py
+++ sampleDataLib.py
@@ -36,9 +36,9 @@ figConverter = figureText.Text2FigConverter(conversionType=FIG_CONVERSION,
 						numWords=FIG_CONVERSION_NWORDS)
 #-----------------------------------
 
-class ClassifiedSampleSet (object):
+class SampleSet (object):
     """
-    IS:     a set of ClassifiedSamples records
+    IS:     a set of Samples
     HAS:    sample record list, ...
     DOES:   Loads/parses sample records from a sample record file
 	    Writes sample records to a file
@@ -55,17 +55,9 @@ class ClassifiedSampleSet (object):
 	For now, lets not worry and hope garbage collection takes care of 
 	things ok.
     """
-    def __init__(self, sampleObjType=None):
+    def __init__(self, sampleObjType):
+	self.sampleObjType = sampleObjType
 	self.samples = []
-	self.numPositives = 0
-	self.numNegatives = 0
-	self.journals     = set()   # set of all journal names in the samples
-				   
-	if sampleObjType: # The python class of sample objects
-	    self.sampleObjType = sampleObjType
-	else:		# get from config
-	    self.sampleObjType = \
-			    getattr(sys.modules[__name__], SAMPLE_OBJ_TYPE_NAME)
     #-------------------------
 
     def read(self, inFile,	# file pathname or open file obj for reading
@@ -87,7 +79,6 @@ class ClassifiedSampleSet (object):
 	del rcds[-1]            # empty string after end of split
 
 	for sr in rcds:
-	    			# need to know which class to instantiate
 	    self.addSample( self.sampleObjType().parseSampleRecordText(sr) )
 	return self
     #-------------------------
@@ -126,29 +117,63 @@ class ClassifiedSampleSet (object):
 	if not isinstance(sample, self.sampleObjType):
 	    raise TypeError('Invalid sample type %s' % str(type(sample)))
 	self.samples.append(sample)
+	return self
+    #-------------------------
+
+    def getSamples(self):	return self.samples
+
+    def getSampleIDs(self):
+	return [s.getID() for s in self.samples]
+    def getDocuments(self):
+	return [s.getDocument() for s in self.samples]
+
+    def getNumSamples(self):	return len(self.samples)
+    def getRecordEnd(self):	return RECORDEND
+
+    def getSampleObjType(self): return self.sampleObjType
+    def getSampleClassNames(self):
+	return self.sampleObjType.getClassNames()
+
+    def getY_positive(self):	return self.sampleObjType.getY_positive()
+    def getY_negative(self):	return self.sampleObjType.getY_negative()
+
+    def getFieldNames(self):	return self.sampleObjType.getFieldNames()
+    def getHeaderLine(self):	return self.sampleObjType.getHeaderLine()
+
+# end class SampleSet -----------------------------------
+
+class ClassifiedSampleSet (SampleSet):
+    """
+    IS:     a SampleSet of ClassifiedSamples
+    HAS:    sample record list, list of journals, counts of pos/neg samples
+    """
+    def __init__(self, sampleObjType=None):
+	if not sampleObjType:	# get from config
+	    self.sampleObjType = \
+			    getattr(sys.modules[__name__], SAMPLE_OBJ_TYPE_NAME)
+	else: self.sampleObjType = sampleObjType
+
+	SampleSet.__init__(self, self.sampleObjType)
+	self.numPositives = 0
+	self.numNegatives = 0
+	self.journals     = set()   # set of all journal names in the samples
+    #-------------------------
+
+    def addSample(self, sample,		# ClassifiedSample
+	):
+	SampleSet.addSample(self, sample)
 	if sample.isPositive(): self.numPositives += 1
 	else:                   self.numNegatives += 1
 	self.journals.add(sample.getJournal())
 	return self
     #-------------------------
 
-    def getSamples(self):	return self.samples
-    def getNumSamples(self):	return len(self.samples)
     def getNumPositives(self):	return self.numPositives
     def getNumNegatives(self):	return self.numNegatives
     def getJournals(self):	return self.journals	# set of names
-    def getRecordEnd(self):	return RECORDEND
 
-    def getSampleObjType(self): return self.sampleObjType
-    def getSampleClassNames(self):
-	return self.sampleObjType.getSampleClassNames()
-    def getY_positive(self):	return self.sampleObjType.getY_positive()
-    def getY_negative(self):	return self.sampleObjType.getY_negative()
-
-    def getHeaderLine(self):	return self.sampleObjType.getHeaderLine()
     def getExtraInfoFieldNames(self):
 	return self.sampleObjType.getExtraInfoFieldNames()
-
 # end class ClassifiedSampleSet -----------------------------------
 
 #-----------------------------------
@@ -165,27 +190,115 @@ class BaseSample (object):
     A training sample has a known class that it belongs to,
     A sample to predict does not have a known class
 
+    HAS: ID, title, abstract, extracted text
+
     Provides various methods to preprocess a sample record
     (preprocess the text prior to vectorization)
+
+    Assumes the fields of Sample subclasses are stored as a dict of
+	    {fieldname: value}
+
     JIM: think about refactoring this class into a (real) BaseSample that lives
 	in MLtextTools and BaseArticleSample that lives in sampleDataLib.
 	Then (Un)ClassifiedSample would inherit from BaseArticleSample
     """
+		# I think these need to be in alpha order if you
+		#  load samples using sklearn's load_files() function.
+		#  (and they need to match the directory names where the
+		#  sample files are stored)
+    sampleClassNames = ['no','yes']
+    y_positive = 1	# sampleClassNames[y_positive] is the "positive" class
+    y_negative = 0	# ... negative
+
+    # fields of a sample as an input/output record (as text), in order
+    fieldNames = [ \
+	    'ID'            ,
+	    'journal'       ,
+	    'title'         ,
+	    'abstract'      ,
+	    'extractedText' ,
+	    ]
+    fieldSep = FIELDSEP
+
     def __init__(self,):
 	pass
+    #----------------------
+
+    def parseSampleRecordText(self, text):
+	"""
+	Parse the text representing a sample record and populate self
+	with that record
+	"""
+	values = {}
+	fields = text.split(self.fieldSep)
+
+	for i, fn in enumerate(self.fieldNames):
+	    values[ fn ] = fields[i]
+
+	return self.setFields(values)
+    #----------------------
+
+    def getSampleAsText(self):
+	""" Return this record as a text string
+	"""
+	return self.fieldSep.join([ self.values[fn] for fn in self.fieldNames])
+    #----------------------
+
+    def setFields(self, values,		# dict
+	):
+	self.values = { fn: str(values.get(fn,'')) for fn in self.fieldNames }
+	return self
     #----------------------
 
     def constructDoc(self):
 	return '\n'.join([self.getTitle(), self.getAbstract(),
 						    self.getExtractedText()])
-
+    def getDocument(self):	return self.constructDoc()
     #----------------------
+
+    def setID(self, t):		self.values['ID'] = t
+    def getID(self,  ):		return self.values['ID']
     def getSampleName(self):	return self.getID()
     def getSampleID(self):	return self.getID()
     def getName(self):		return self.getID()
-
-    def getDocument(self):	return self.constructDoc()
     #----------------------
+
+    def setExtractedText(self, t): self.values['extractedText'] = t
+    def getExtractedText(self,  ): return self.values['extractedText']
+
+    def setAbstract(self, t): self.values['abstract'] = t
+    def getAbstract(self,  ): return self.values['abstract']
+
+    def setTitle(self, t): self.values['title'] = t
+    def getTitle(self,  ): return self.values['title']
+
+    def getJournal(self):  return self.values['journal']
+
+    #----------------------
+
+    @classmethod
+    def getHeaderLine(cls):	return cls.fieldSep.join( cls.fieldNames)
+
+    @classmethod
+    def getFieldSep(cls):	return cls.fieldSep
+
+    @classmethod
+    def getFieldNames(cls):	return cls.fieldNames
+
+    @classmethod
+    def getClassNames(cls):	return cls.sampleClassNames
+
+    @classmethod
+    def getY_positive(cls):	return cls.y_positive
+
+    @classmethod
+    def getY_negative(cls):	return cls.y_negative
+
+    #----------------------
+    # preprocessSamples.py script checks for rejected samples.
+    #  For autolittriage, we don't have any checks to reject samples (yet)
+    def isReject(self):		return False
+    def getRejectReason(self):	return None
 
     #----------------------
     # "preprocessor" functions.
@@ -282,7 +395,8 @@ class BaseSample (object):
 	
 	self.setTitle( self.getTitle()[:10].replace('\n',' ') )
 	self.setAbstract( self.getAbstract()[:20].replace('\n',' ') )
-	self.setExtractedText(self.getExtractedText()[:20].replace('\n',' ')+'\n')
+	self.setExtractedText(self.getExtractedText()[:20].replace('\n',' ') \
+									+'\n')
 	return self
     # ---------------------------
 
@@ -293,6 +407,15 @@ class BaseSample (object):
 	self.setAbstract( 'abstract...' )
 	self.setExtractedText( 'extracted text...\n' )
 	return self
+    # ---------------------------
+
+    def addJournalFeature(self):		# preprocessor
+	''' Add the journal name as a text token to the document
+	'''
+	jtext = 'journal__' + '_'.join( self.getJournal().split(' ') ).lower()
+	self.setExtractedText( self.getExtractedText() + " " + jtext )
+	return self
+    # ---------------------------
 # end class BaseSample ------------------------
 
 class ClassifiedSample (BaseSample):
@@ -306,55 +429,27 @@ class ClassifiedSample (BaseSample):
 	be used to subset the sample set when analyzing prediction results.
 	(e.g., if we want to analyze precision/recall for individual journals)
     """
-		# I think these need to be in alpha order if you
-		#  load samples using sklearn's load_files() function.
-		#  (and they need to match the directory names where the
-		#  sample files are stored)
-    sampleClassNames = ['no','yes']
-    y_positive = 1	# sampleClassNames[y_positive] is the "positive" class
-    y_negative = 0	# ... negative
 
     # fields of a sample as an input/output record (as text), in order
     # Need to be specified by subclasses
-    fieldNames = [  ]
+    # fields of a sample as an input/output record (as text), in order
+    fieldNames = [ \
+	    'knownClassName',
+	    'ID'            ,
+	    'journal'       ,
+	    'title'         ,
+	    'abstract'      ,
+	    'extractedText' ,
+	    ]
     extraInfoFieldNames = [  ] # should be [] if no extraInfoFields
-
-    fieldSep = FIELDSEP
 
     def __init__(self,):
 	BaseSample.__init__(self)
     #----------------------
 
-    def parseSampleRecordText(self, text):
-	"""
-	Parse the text representing a sample record and populate self
-	with that record
-	"""
-	values = {}
-	fields = text.split(self.fieldSep)
-
-	for i, fn in enumerate(self.fieldNames):
-	    values[ fn ] = fields[i]
-
-	return self.setFields(values)
-    #----------------------
-
-    def getSampleAsText(self):
-	""" Return this record as a text string
-	"""
-	return self.fieldSep.join([ self.values[fn] for fn in self.fieldNames])
-    #----------------------
-
-    @classmethod
-    def getHeaderLine(cls):
-	""" Return sample output file column header line
-	"""
-	return cls.fieldSep.join( cls.fieldNames)
-    #----------------------
-
     def setFields(self, values,		# dict
 	):
-	self.values = { fn: str(values[fn]) for fn in self.fieldNames }
+	BaseSample.setFields(self,values)
 	self.setKnownClassName( values['knownClassName'] )
 	return self
     #----------------------
@@ -390,31 +485,7 @@ class ClassifiedSample (BaseSample):
 	return self.getKnownYvalue() == self.y_positive 
     def isNegative(self):
 	return not self.isPositive()
-
-    def setID(self, t): self.values['ID'] = t
-    def getID(self,  ): return self.values['ID']
-
-    def setExtractedText(self, t): self.values['extractedText'] = t
-    def getExtractedText(self,  ): return self.values['extractedText']
-
-    def setAbstract(self, t): self.values['abstract'] = t
-    def getAbstract(self,  ): return self.values['abstract']
-
-    def setTitle(self, t): self.values['title'] = t
-    def getTitle(self,  ): return self.values['title']
-
-    def getJournal(self):  return self.values['journal']
-
     #----------------------
-    @classmethod
-    def getSampleClassNames(cls): return cls.sampleClassNames
-    @classmethod
-    def getY_positive(cls): return cls.y_positive
-    @classmethod
-    def getY_negative(cls): return cls.y_negative
-    #----------------------
-    @classmethod
-    def getFieldSep(cls):   return cls.fieldSep
     @classmethod
     def getExtraInfoFieldNames(cls): return cls.extraInfoFieldNames
     def getExtraInfo(self):
@@ -427,24 +498,6 @@ class ClassifiedSample (BaseSample):
 	self.extraInfo['abstractLen'] = str( len(self.getAbstract()) )
 	self.extraInfo['textLen']     = str( len(self.getExtractedText()) )
     #----------------------
-
-    # preprocessSamples.py script checks for rejected samples.
-    #  For autolittriage, we don't have any checks to reject samples (yet)
-    def isReject(self):		return False
-    def getRejectReason(self):	return None
-
-    #----------------------
-    # "preprocessor" functions.
-    #  Each preprocessor should modify this sample and return itself
-    #----------------------
-
-    def addJournalFeature(self):		# preprocessor
-	''' Add the journal name as a text token to the document
-	'''
-	jtext = 'journal__' + '_'.join( self.getJournal().split(' ') ).lower()
-	self.setExtractedText( self.getExtractedText() + " " + jtext )
-	return self
-    # ---------------------------
 # end class ClassifiedSample ------------------------
 
 class PrimTriageClassifiedSample(ClassifiedSample):
@@ -539,23 +592,58 @@ class CurGroupClassifiedSample(ClassifiedSample):
 	    ]
 # end class CurGroupClassifiedSample ------------------------
 
+class PrimTriageUnClassifiedSample(BaseSample):
+    """
+    Represents a sample document to predict
+    """
+    sampleClassNames = ['discard','keep']
+    y_positive = 1
+    y_negative = 0
 
-class UnclassifiedSample (BaseSample):
+    # fields of a sample as an input/output record (as text), in order
+    fieldNames = [ \
+	    'ID'            ,
+	    'isReview'      ,
+	    'refType'       ,
+	    'journal'       ,
+	    'title'         ,
+	    'abstract'      ,
+	    'extractedText' ,
+	    ]
+# end class PrimTriageUnClassifiedSample ------------------------
+
+class CurGroupUnClassifiedSample(BaseSample):
     """
-    IS a sample record that we need to predict (i.e., a new, unseen article)
-    Will fill this out when we get to predicting new articles
+    Represents a sample document to predict
     """
-    pass
-# end class UnclassifiedSample ------------------------
+    sampleClassNames = ['selected','unselected']
+    y_positive = 0
+    y_negative = 1
+
+    # fields of a sample as an input/output record (as text), in order
+    fieldNames = [ \
+	    'ID'            ,
+	    'isReview'      ,
+	    'refType'       ,
+	    'journal'       ,
+	    'title'         ,
+	    'abstract'      ,
+	    'extractedText' ,
+	    ]
+# end class CurGroupUnClassifiedSample ------------------------
 
 if __name__ == "__main__":	# ad hoc test code
-    ss = ClassifiedSampleSet(sampleObjType=PrimTriageClassifiedSample)
-    print ss.getExtraInfoFieldNames()
-    print ss.getSampleObjType()
-    print ss.getSampleClassNames()
-    print ss.getY_positive()
-    print ss.getY_negative()
-    if False:	# basic CurGroupClassified Sample tests
+    if True:
+	print "----------------------"
+	print "ClassifiedSampleSet PrimTriageClassifiedSample tests\n"
+	ss = ClassifiedSampleSet(sampleObjType=PrimTriageClassifiedSample)
+	print ss.getExtraInfoFieldNames()
+	print ss.getSampleObjType()
+	print ss.getFieldNames()
+	print ss.getSampleClassNames()
+	print ss.getY_positive()
+	print ss.getY_negative()
+    if True:	# basic CurGroupClassified Sample tests
 	r3 = CurGroupClassifiedSample().parseSampleRecordText(\
     '''unselected|pmID1|01/01/1900|1900|keep|0|non-peer1|supp type1|apstat1|gxdstat1|goStat1|tumorstat1|qtlStat1|Journal of Insomnia|My Title|
     My Abstract|My text: it's a knock out https://foo text www.foo.org word word  -/- the final words'''
@@ -613,6 +701,11 @@ if __name__ == "__main__":	# ad hoc test code
 	print
 
     if True: # ClassifiedSampleSet (for PrimTriageClassifiedSample) tests
+	r2 = PrimTriageClassifiedSample().parseSampleRecordText(\
+	''';discard|pmID1|10/3/2017|1901|1|peer2|supp2|apstat2|gxdStat2|goStat2|tumorStat2|qtlStat2|journal2|title2|abstract2|text2''')
+	r1 = PrimTriageClassifiedSample().parseSampleRecordText(\
+	'''keep|pmID1|01/01/1900|1900|0|non-peer1|supp type1|apstat1|gxdstat1|goStat1|tumorstat1|qtlStat1|Journal of Insomnia|My Title|
+	My Abstract|My text: it's a knock out https://foo text www.foo.org word word  -/- the final words''')
 	print "---------------"
 	print "SampleSet tests\n"
 	print r2.getKnownClassName()
@@ -641,6 +734,29 @@ if __name__ == "__main__":	# ad hoc test code
 	ss.write(sys.stdout)
 	print "\nEnd Output file"
 	
+    if True:	# PrimTriageUnClassified Sample tests
+	r2 = PrimTriageUnClassifiedSample().parseSampleRecordText(\
+		'''pmID1|1|peer2|journal2|title2|abstract2|text2''')
+	r1 = PrimTriageUnClassifiedSample().parseSampleRecordText(\
+		'''pmID1|1|non-peer1|journal1|title1|abstract1|text1''')
+	
+	print "----------------------"
+	print "PrimTriageUnClassifiedSample tests\n"
+	print "SampleName: '%s'"	% r1.getSampleName()
+	print "Journal: '%s'"		% r1.getJournal()
+	print "title: \n'%s'\n"		% r1.getTitle()
+	print "abstract: \n'%s'\n"	% r1.getAbstract()
+	print "extractedText: \n'%s'\n"	% r1.getExtractedText()
+	print "document: \n'%s'\n"	% r1.getDocument()
+	print "----------------------"
+	print "SampleSet PrimTriageUnClassifiedSample tests\n"
+	ss = SampleSet(sampleObjType=PrimTriageUnClassifiedSample)
+	ss.addSamples([r1, r2])
+	print ss.getSampleObjType()
+	print ss.getFieldNames()
+	print ss.getHeaderLine()
+	print ss.getSamples()
+
     if False:		# preprocessor tests
 	print "---------------"
 	print "Preprocessor tests\n"
